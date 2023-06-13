8290010e918d42a9d6b0d93b63d27a90bc25d66d
Sourabh Sinha <Sourabh>
2023-06-13 14:11:28 +0530

    implement generator to query, using Dependency Injection

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,3 +1,4 @@
+from typing import Generator 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -8,5 +9,21 @@ SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL is ",SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
+
+#if you don't want to install postgres or any database, use sqlite, a file system based database, 
+# uncomment below lines if you would like to use sqlite and comment above 2 lines of SQLALCHEMY_DATABASE_URL AND engine
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
+
+def get_db() -> Generator:  
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
