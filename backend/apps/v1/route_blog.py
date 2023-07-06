159fef976bd2b7d3d754143cdd248d98d3d4f813
sourabhsinha396 <sourabh@chat360.io>
2023-07-06 16:35:18 +0530

    blog detail view for app

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,4 +1,5 @@
 from db.repository.blog import list_blogs
+from db.repository.blog import retreive_blog
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
@@ -15,4 +16,12 @@ def home(request: Request, db: Session = Depends(get_db)):
     blogs = list_blogs(db=db)
     return templates.TemplateResponse(
         "blog/home.html", {"request": request, "blogs": blogs}
+    )
+
+
+@router.get("/app/blog/{id}")
+def blog_detail(request: Request, id: int, db: Session = Depends(get_db)):
+    blog = retreive_blog(id=id, db=db)
+    return templates.TemplateResponse(
+        "blog/detail.html", {"request": request, "blog": blog}
     )
